060be46e25922243058c3e3514c4bb05826f80e8
Aniket Yadav <152779209+Aniketyadav9026@users.noreply.github.com>
2024-07-14 10:54:40 +0000

    14/07/24

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 142389;
+let accountEmail = "Aniket@google.com"
+var accountPassword = "12345"
+accountcity = "jaipur"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "kavi@google.com"
+accountPassword = "902635"
+accountcity = "Bengaluru"
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+
+/*
+Prefer not to use Var
+becouse of issue in block scope and function scope.
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountcity, accountState])
+
