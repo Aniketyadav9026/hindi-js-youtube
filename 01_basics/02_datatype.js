5b4816c6396da8741f0e77e05cc088ca807c83a3
Aniket Yadav <152779209+Aniketyadav9026@users.noreply.github.com>
2024-07-14 11:16:52 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,21 @@
+let name = "aniket"
+let age = 14;
+let online = true;
+
+// primitive datatype
+
+// Number = 2 to power 53
+// string
+// bigint
+// boolean
+// null 
+// undefined
+// symbol
+
+// non primitive datatype
+
+// object
+// array
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
