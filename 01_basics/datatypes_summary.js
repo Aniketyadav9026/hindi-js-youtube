b4a4cff58f28dc65d5494457bf6412d7eb7f1889
Aniket Yadav <152779209+Aniketyadav9026@users.noreply.github.com>
2024-08-11 16:00:26 +0000

    Datatype Summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,12 @@
+// javascript is a dynamic type language. because we don't need to write datatype name.
+
+
+// primitive
+
+// string, Number, null, undefined, symbol, boolean, bigInt.
+
+// reference (non-primitive)
+
+// Array, Objects, function.
+
+// typeof null => OBJECT
